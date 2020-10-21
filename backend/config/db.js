345ff679f28ec0f8090fe23632470ca7ec260030
Rilwan Yusuf <ryusufu@gmail.com>
2020-10-21 11:08:18 +0100

    installed Mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const conn = await 
+    } catch (error) {
+        
+    }
+}
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({match}) => {
-    const product = products.find((p) => p._id === match.params.id)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+       const fetchProduct = async () => {
+           const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+           setProduct(data)
+       }
+
+       fetchProduct()
+
+    }, [match])
+
     return (
     <>
         <Link className="btn btn-dark my-3" to="/">
@@ -22,7 +34,7 @@ const ProductScreen = ({match}) => {
                     </ListGroup.Item>
                     
                     <ListGroup.Item>
-                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        <Rating key={product._id} value={product.rating} text={`${product.numReviews} reviews`} />
                     </ListGroup.Item>
                     <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
                     <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
