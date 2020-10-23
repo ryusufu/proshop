3e1b957e48fc59c396066cc0d76b60751a1b7fdc
Rilwan Yusuf <ryusufu@gmail.com>
2020-10-23 08:04:30 +0100

    added bcrypt

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,19 @@
-import mongoose from 'mongoose'
+const mongoose = require('mongoose') 
 
 const connectDB = async () => {
     try {
-        const conn = await 
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
-        
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
     }
+    
 }
+
+module.exports = connectDB
---
+++ backend/data/users.js
@@ -0,0 +1,21 @@
+const bcryptjs = require("bcryptjs")
+
+const users = [
+    {
+        name: "Admin User",
+        email: "admin@example.com",
+        password: bcryptjs.hashSync("123456", 10),
+        isAdmin: true
+    },
+    {
+        name: "John Doe",
+        email: "admin@example.com",
+        password: bcryptjs.hashSync("123456", 10)
+    },
+    {
+        name: "Jane Doe",
+        email: "admin@example.com",
+        password: bcryptjs.hashSync("123456", 10)
+    },
+
+]
